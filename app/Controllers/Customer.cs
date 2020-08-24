6f903bc69461dce683ae63c9713b0368bd71e792
chatapazar <31641737+chatapazar@users.noreply.github.com>
2020-08-24 21:53:21 +0700

    Add files via upload

---
+++ app/Controllers/Customer.cs
@@ -0,0 +1,9 @@
+namespace Test.Models
+{
+    public class Customer
+    {
+        public string CustName { get; set; }
+        public string CustNo { get; set; }
+        public string PolicyNo { get; set; }
+    }
+}
---
+++ app/Controllers/CustomersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        // GET api/customers
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> Get()
+        {
+            //return new string[] { "chatchai", "surachai", "kittichade" };
+            
+            Customer[] C = new Customer[3];
+            C[0] = new Customer { CustName  = "chatchai kongmanee", CustNo = "2020080001", PolicyNo = "20200800001"};
+			C[1] = new Customer { CustName  = "surachai kongmanee", CustNo = "2020080002", PolicyNo = "20200800002"};
+            C[2] = new Customer { CustName  = "sirikwan kongmanee", CustNo = "2020080003", PolicyNo = "20200800003"};
+            
+			return C;
+        }
+
+        // GET api/customers/5
+        [HttpGet("{id}")]
+        public ActionResult<Customer> Get(int id)
+        {
+            return new Customer { CustName  = "chatchai kongmanee", CustNo = "2020080001", PolicyNo = "20200800001"};
+        }
+
+    }
+}
