b134a0ed26ed8f6e16baadec62037b0fd20e88c8
chatapazar <31641737+chatapazar@users.noreply.github.com>
2020-08-24 22:47:48 +0700

    Update Startup.cs

--- app/Startup.cs
+++ app/Startup.cs
@@ -29,7 +29,7 @@ namespace app
         public void ConfigureServices(IServiceCollection services)
         {
             //services.AddControllersWithViews();
-          
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
