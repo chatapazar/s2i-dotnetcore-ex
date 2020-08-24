a1732c458ea84363d3e1486c910165086bd41d68
chatapazar <31641737+chatapazar@users.noreply.github.com>
2020-08-24 22:15:42 +0700

    Update Startup.cs

--- app/Startup.cs
+++ app/Startup.cs
@@ -5,9 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace app
 {
@@ -24,6 +28,16 @@ namespace app
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+          
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+            
+            services.AddCors(c =>  
+            {  
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());  
+            }); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +59,16 @@ namespace app
             app.UseRouting();
 
             app.UseAuthorization();
-
+          
+            app.UseSwagger();
+            app.UseCors(options => options.AllowAnyOrigin());  
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+          
+            app.UseMvc();
+          
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
