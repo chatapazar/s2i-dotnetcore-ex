064c3a11a3a227f28e8e4168d02c99f451c5a6a7
chatapazar <31641737+chatapazar@users.noreply.github.com>
2020-08-24 23:18:47 +0700

    Update Program.cs

--- app/Program.cs
+++ app/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace app
+namespace TodoApi
 {
     public class Program
     {
@@ -20,7 +20,8 @@ namespace app
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    //webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<StartupJavaScript>();
                 });
     }
 }
